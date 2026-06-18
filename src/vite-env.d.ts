3939a2b3687b89a136b7db9538cd9716fb2481af
makoragi <makoragi@gmail.com>
2026-06-18 20:27:56 +0900

    Add GTFS-RT config and local preview docs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 declare module '*.css'
+
+interface ImportMetaEnv {
+  readonly VITE_GTFS_RT_API_KEY?: string
+  readonly VITE_GTFS_RT_API_KEY_HEADER?: string
+  readonly VITE_GTFS_RT_AUTH_TOKEN?: string
+  readonly VITE_GTFS_RT_ALERTS_URL?: string
+  readonly VITE_GTFS_RT_TRIP_UPDATES_URL?: string
+  readonly VITE_GTFS_RT_USE_MOCK?: string
+  readonly VITE_GTFS_RT_VEHICLE_POSITIONS_URL?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
